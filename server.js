10808478d12ed1ff12d21d6867343cdb90f8ae30
luvkushsynghh <lavkush.singh@st.niituniversity.in>
2016-10-17 01:45:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,55 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article one | Luvkush',
+    heading: 'Coding in HTML',
+    date: 'October 17,2016',
+    content: `
+            <p title="Read instead of mouseover">
+                This is my html page from github via IMAD hasura interface. This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface. 
+            </p>
+            <p title="Second para">
+                This is my html page from github via IMAD hasura interface. This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface. 
+            </p>
+            <p title="Read instead of mouseover, third para">
+                This is my html page from github via IMAD hasura interface. This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface.This is my html page from github via IMAD hasura interface. 
+            </p>`
+};
+
+function  createTemplate (data){
+    var title = data.title;
+    var date = data.date;
+    var content = data.content;
+    var heading = data.heading;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title> ${title} </title>
+            <meta name="viewport" content ="width-device-width, initial-scale=1">
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+             <div class="container">
+            <div>
+                <a href="/"> Home </a>
+            </div>
+            <hr/>
+            <h1> ${heading}</h1>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+            </div>
+        </body>
+    </html>`;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-one', function(req,res){
